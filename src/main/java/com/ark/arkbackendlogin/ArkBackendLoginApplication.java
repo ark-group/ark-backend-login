243610bb9ed736578e5a3cc828677c5bcabb9306
krrathore1990 <karanr15.rathore@gmail.com>
2019-04-27 23:52:29 +0530

    first commit

---
+++ src/main/java/com/ark/arkbackendlogin/ArkBackendLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ark.arkbackendlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkBackendLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkBackendLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ark/arkbackendlogin/ArkBackendLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ark.arkbackendlogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArkBackendLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
